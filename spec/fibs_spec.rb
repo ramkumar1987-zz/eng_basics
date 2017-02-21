4798ea0e6371c4a8da612221a1109ae926d986a9
Ramkumar Ramanathan <ramkumar.ramanathan1987@gmail.com>
2017-02-21 11:12:11 -0600

    Update Fibs specs

--- spec/fibs_spec.rb
+++ spec/fibs_spec.rb
@@ -1,76 +1,87 @@
 require_relative '../fibs'
 
 describe Fibs do
-  subject { Fibs.new }
+  let(:subject) { Fibs.new }
 
   describe '#generate' do
-    let(:expected_result) {
-      [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
-    }
+    let(:expected_result) do
+      [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    end
 
     it 'returns the number of requested fibonacci numbers' do
-      expect( subject.generate(10) ).to eq(expected_result)
+      expect(subject.generate(10)).to eq(expected_result)
     end
 
     it 'stores the values' do
       subject.generate(10)
-      expect( subject.generated_fibs ).to eq(expected_result)
+      expect(subject.generated_fibs).to eq(expected_result)
     end
 
     context 'when it already knows the requested number of sequence numbers' do
-      before do
-        subject.generate(5)
-        allow_any_instance_of(Fibs).to receive(:generate_fibs) { raise Exception }
-      end
-
+      # setting an expectation that generated_fibs holds the original fibonacci
+      # sequence despite generating a sequence for a lower value.
       it 'does not call generate_fibs' do
-        # write an expectation for calling generate
+        subject.generate(10)
+        expect(subject.generate(5)).to eq([0, 1, 1, 2, 3])
+        expect(subject.generated_fibs).to eq(expected_result)
       end
     end
   end
 
   describe '#is_known_fib?' do
+    before(:each) do
+      subject.generated_fibs = [0, 1, 1, 1]
+    end
+
     context 'when passed a fibonacci number that it has not stored' do
       it 'returns false' do
+        expect(subject.is_known_fib?(400)).to eq(false)
       end
     end
 
     context 'when passed a number that has been stored' do
       it 'returns true' do
+        expect(subject.is_known_fib?(1)).to eq(true)
       end
     end
   end
 
   describe '#all_fibs?' do
-    context 'when passed an array of numbers that are all members of the the fibonacci sequence' do
+    before(:each) do
+      subject.generated_fibs = [0, 1, 1, 2]
+    end
+
+    context 'when passed an array of numbers that are all members of ' \
+            'the the fibonacci sequence' do
       it 'returns true' do
+        expect(subject.all_fibs?([0, 1, 1, 2])).to eq(true)
       end
     end
 
-    context 'when passed an array of numbers that where at least one is not a member of the sequence' do
+    context 'when passed an array of numbers that where at least one is not ' \
+            'a member of the sequence' do
       it 'returns false' do
+        expect(subject.all_fibs?([0, 1, 1, 1])).to eq(false)
       end
     end
   end
 
   describe '#known_fibs_sum_of_squares' do
-    let(:expected_result) {
-      # the known sum of the squares
-    }
+    let(:expected_result) { 15 }
 
     context 'when there are stored numbers' do
       before do
-        # seed generated_fibs
+        subject.generated_fibs = [0, 1, 1, 2, 3]
       end
 
       it 'returns the sum of the squared values for all known fibs' do
-        expect( subject.known_fibs_sum_of_squares ).to eq( expected_result )
+        expect(subject.known_fibs_sum_of_squares).to eq(expected_result)
       end
     end
 
     context 'when there are no stored numbers' do
       it 'returns 0' do
-        expect( subject.known_fibs_sum_of_squares ).to eq( 0 )
+        expect(subject.known_fibs_sum_of_squares).to eq(0)
       end
     end
   end
